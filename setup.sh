c48122c5d198cb606397eab504559b7d698c0c55
Kinsteen <paul@francon.eu>
2021-09-03 19:21:24 +0200

    Added things

--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
+
+# Install fonts for zsh
+mkdir -p ~/.local/share/fonts
+
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -O ~/.local/share/fonts/MesloLGS NF Regular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -O ~/.local/share/fonts/MesloLGS NF Bold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -O ~/.local/share/fonts/MesloLGS NF Italic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -O ~/.local/share/fonts/MesloLGS NF Bold Italic.ttf
+
+fc-cache -f # Rebuild all font cache
+
 # Install oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O omzsh.sh
+chmod +x omzsh.sh
+CHSH=yes RUNZSH=no sh omzsh.sh --unattended
+rm omzsh.sh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "Linking config files"
 
 ln -sf $(pwd)/alacritty/ ~/.config/
 ln -sf $(pwd)/bspwm ~/.config/
-ln -sf $(pwd)/mpd ~/.config/
+#ln -sf $(pwd)/mpd ~/.config/
 ln -sf $(pwd)/picom ~/.config/
 ln -sf $(pwd)/polybar ~/.config/
 ln -sf $(pwd)/powerlevel10k ~/.config/
